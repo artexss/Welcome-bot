61cf0fa363222bf8a86d44e278f9e7ccdb028fe7
! Art <124545712+artexss@users.noreply.github.com>
2024-04-14 17:43:15 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+const config = require("./config.js");
+const Discord = require("discord.js");
+//exports.token = "";
+//exports.mongodb = "";
+
+
+exports.owners = ["555562270222974996" , "1120034661607936132" ];
+
+
+
+
+
+
+exports.status = 'idle';
+
+exports.activity = {
+  name: 'Bot Welcome .. ',
+  type: 'PLAYING'
+  //url: 'https://www.twitch.tv/YOUR NAME'
+};
+
